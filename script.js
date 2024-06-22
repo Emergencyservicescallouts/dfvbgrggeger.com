156c70e4a16ebc0b65ffbe1b159a29835f284538
Emergencyservicescallouts <167202034+Emergencyservicescallouts@users.noreply.github.com>
2024-06-22 16:35:30 +1000

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let unlockedUnits = {
-    medicUnit: false,
-    aerialUnit: false
-};
-
-function unlockSpecialUnit(unitType) {
-    unlockedUnits[unitType] = true;
-    // Update UI to reflect unlocked status
-}
